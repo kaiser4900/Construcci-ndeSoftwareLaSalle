f2f9f57bd9590acfcecd55eb67af8a2b37392d42
Gamis777 <42852437+Gamis777@users.noreply.github.com>
2021-03-23 19:08:40 -0500

    Funciones del Controlador  Establecido

--- hello_world/app/Http/Controllers/IncidenciaController.php
+++ hello_world/app/Http/Controllers/IncidenciaController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\incidencia;
+use App\Models\Incidencia;
 use Illuminate\Http\Request;
 
 class IncidenciaController extends Controller
@@ -14,7 +14,8 @@ class IncidenciaController extends Controller
      */
     public function index()
     {
-        //
+        $incidencias = Incidencia::all();
+        return view('incidencias',compact('incidencias'));
     }
 
     /**
@@ -24,7 +25,8 @@ class IncidenciaController extends Controller
      */
     public function create()
     {
-        //
+        $incidencias = Incidencia::all();
+        return view('incidencias_add',compact('incidencias'));
     }
 
     /**
@@ -35,7 +37,17 @@ class IncidenciaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'email' => 'required',
+            'tema' => 'required',
+            'detalle' => 'required',
+        ]);
+        $nueva_incidencia = new Incidencia();
+        $nueva_incidencia->email = $request->email;
+        $nueva_incidencia->tema = $request->tema;
+        $nueva_incidencia->detalle = $request->detalle;
+        $nueva_incidencia->save();
+        return back()->with('mensaje', 'Incidencia Agregada');
     }
 
     /**
@@ -78,8 +90,11 @@ class IncidenciaController extends Controller
      * @param  \App\Models\incidencia  $incidencia
      * @return \Illuminate\Http\Response
      */
-    public function destroy(incidencia $incidencia)
+    public function destroy($id)
     {
-        //
+        $incidencia =Incidencia::findOrFail($id);
+        $incidencia->delete();
+    
+        return back()->with('mensaje', 'Incidencia Eliminada');
     }
 }
